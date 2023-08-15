7292e49af4a51839c0fce946743c598d24e76e97
touraymk <131713898+touraymk@users.noreply.github.com>
2023-08-15 18:15:02 +0200

    Additional styling with CSS

--- src/Weather.js
+++ src/Weather.js
@@ -5,13 +5,14 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <form>
-        <input type="search" placeholder="Enter city.." className="col-7" />
-        <button type="submit">🔎</button>
-        <button type="button">📍</button>
-      </form>
-
-      <div className="row mt-4">
+      <div className="mb-4">
+        <form>
+          <input type="search" placeholder="Enter city.." className="col-7" />
+          <button type="submit">🔎</button>
+          <button type="button">📍</button>
+        </form>
+      </div>
+      <div className="row">
         <div className="m-3">
           <h1>Bridgetown</h1>
           <p>Bridgetown, Monday 22:00</p>
