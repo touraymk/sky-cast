5a0fd9f99a0cae685a61c26f09cd3840612cae97
touraymk <131713898+touraymk@users.noreply.github.com>
2023-08-15 16:48:06 +0200

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Hello world</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
