ede1a5a193896efc052ad2bb021124262c8a75d1
touraymk <131713898+touraymk@users.noreply.github.com>
2023-08-15 17:47:25 +0200

    Added content
    Added content to App.js and Weather.js and styled it with CSS and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Weather from "./Weather";
+// import videoFile from "./pink-bg.mp4";
 
 import "./App.css";
 
@@ -8,6 +9,24 @@ function App() {
       <div className="container">
         <Weather />
       </div>
+      <footer className="text-center">
+        Coded with <span className="emojiColor">♥️</span> by{" "}
+        <a
+          href="https://monumental-malasada-41cd9f.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Mariama Touray
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/touraymk/sky-cast"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -5,7 +5,18 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <h2>Hello from weather</h2>
+      <form>
+        <input type="search" placeholder="Enter city.." className="col-7" />
+        <button type="submit">🔎</button>
+        <button type="button">📍</button>
+      </form>
+
+      <div className="row mt-4">
+        <div className="m-3">
+          <h1>Bridgetown</h1>
+          <p>Bridgetown, Monday 22:00</p>
+        </div>
+      </div>
     </div>
   );
 }
