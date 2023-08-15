02bba370f3f06eb7724c11f9fa700443c4f52f8f
touraymk <131713898+touraymk@users.noreply.github.com>
2023-08-15 21:07:08 +0200

    Replaced temp with live data, styling with css

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,11 +2,11 @@ import React from "react";
 
 import "./WeatherTemperature.css";
 
-export default function WeatherTemperature() {
+export default function WeatherTemperature(props) {
   return (
     <div className="WeatherTemperature">
       <h2 className="d-flex">
-        32 <span className="units">&deg;C |</span>
+        {Math.round(props.celsius)} <span className="units">&deg;C |</span>
         <span className="units">&deg;F</span>
       </h2>
     </div>
--- src/WeatherUpdate.js
+++ src/WeatherUpdate.js
@@ -3,14 +3,14 @@ import React from "react";
 import "./WeatherUpdate.css";
 import WeatherTemperature from "./WeatherTemperature";
 
-export default function WeatherUpdate() {
+export default function WeatherUpdate(props) {
   return (
     <div className="WeatherUpdate d-flex">
       <div className="col col-md-6 d-flex">
         <div>
           <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="" />
         </div>
-        <WeatherTemperature />
+        <WeatherTemperature celsius={props.data.temperature} />
       </div>
       <div className="col col-6">
         <ul>
